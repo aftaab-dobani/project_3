548713f17064826db4820548442538538da3cb5e
ant <nivaniucc@gmail.com>
2021-05-26 13:01:17 -0400

    Removed duplicates and compilation errors.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Login/Nav'
 import Jumbotron from './components/Login/Jumbotron'
 import Login from './components/Login/Login'
 
 
+// function App() {
+//   const adminUser = {
+//     email: "admin@admin.com", 
+//     password : "admin1234"
+//   }
+
+//   const [user, setUser] = useState({name:"", email:""});
+//   const [error, setError ] = useState("");
+
+//   const Login = details => {
+//     console.log(details);
+//   }
+
+//   const Logout = () => {
+//     console.log("Logout");
+//   }
+
 function App() {
-  const adminUser = {
-    email: "admin@admin.com", 
-    password : "admin1234"
-  }
-
-  const [user, setUser] = useState({name:"", email:""});
-  const [error, setError ] = useState("");
-
-  const Login = details => {
-    console.log(details);
-  }
-
-  const Logout = () => {
-    console.log("Logout");
-  }
-
   return (
     <div className="App">
       <Nav />
